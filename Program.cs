2b7245aac1baccb0bbfd40297d6b6823e9d23d97
TaHuC <TaHuC@hotmail.com>
2017-01-30 09:20:51 +0200

    Get triagle area is finish

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculateTriangleArea
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double width = double.Parse(Console.ReadLine());
+			double height = double.Parse(Console.ReadLine());
+
+			double area = GetTriagleArea(width, height);
+
+			Console.WriteLine(area);
+
+		}
+
+		public static double GetTriagleArea(double width, double height)
+		{
+			return width * height / 2;
+		}
+	}
+}
